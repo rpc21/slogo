8aeb175f7cf183be5c0c5b2c903207e9fceaf30a
Megan Phibbons <mep76@duke.edu>
2019-02-24 14:45:30 -0500

    added in functionality for multiple commands at once

--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,5 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class Parser {
@@ -12,54 +9,42 @@ public class Parser {
     private static final String PARAMETER_PROPERTIES_LOCATION = "resources/languages/English.properties";
     private static final String COMMAND_PROPERTIES_LOCATION = "resources/parser/Parameters.properties";
 
+    private String myCurrentCommand;
+
     public Parser() {
         myCommandFactory = new CommandFactory();
     }
 
     public List<CommandNode> parse(String input) { // todo: throw invalidcommandexception and invalidnumberinputs exception
+        myCurrentCommand = input;
         List<CommandNode> topLevelCommands = new ArrayList<>();
         parameterProperties = ResourceBundle.getBundle(PARAMETER_PROPERTIES_LOCATION);
         commandProperties = ResourceBundle.getBundle(COMMAND_PROPERTIES_LOCATION);
-        for(String topLevelCommand : separateTopLevelCommands(input)) {
-            topLevelCommands.add(makeTopLevelCommand(topLevelCommand));
+        while(myCurrentCommand.length() > 0) {
+            topLevelCommands.add(makeNodeTree());
         }
         return topLevelCommands;
     }
 
-    private CommandNode makeTopLevelCommand(String command) {
-        String commandName = command.split("\\s+")[0];
-        int numberOfParameters = Integer.parseInt(parameterProperties.getString(commandName));
-        return makeNode(commandName, numberOfParameters);
-    }
-
-    // purpose: take in all of the commands and divide them up by their top level commands
-    private String[] separateTopLevelCommands(String input) {
-        // todo: go through and look at command and arguments, when each command in a hierarchy has enough arguments, this is one top level command. Then move on
-        return null;
-    }
-
-    private CommandNode makeNode(String command, int expectedNumberOfParameters) {
-        if(command == "") {
-            return null; // end of recursion
-        }
-        String[] commandSplit = command.split("\\s+");
-        if(commandSplit.length < expectedNumberOfParameters + 1) {
-            // todo: throw an exception
-        }
+    private CommandNode makeNodeTree() { // todo: make this better
+        String[] commandSplit = myCurrentCommand.split("\\s+");
+        String currentCommandString = commandSplit[0];
+        int expectedNumberOfParameters = Integer.parseInt(parameterProperties.getString(currentCommandString));
+        myCurrentCommand = myCurrentCommand.substring(currentCommandString.length() + 1);
         CommandNode currentNode = myCommandFactory.makeCommand(commandSplit[0]);
-        command = command.substring(commandSplit[0].length());
         for(int i = 1; i < expectedNumberOfParameters; i++) {
-            if(isDouble(commandSplit[i])) {
-                currentNode.addChild(myCommandFactory.makeCommand(commandSplit[i]));
-            } else {
-                currentNode.addChild(makeNode(command, Integer.parseInt(parameterProperties.getString(commandSplit[i]))));
-            }
-            command = command.substring(commandSplit[i].length()); // todo: does this work? do indices line up?
-        }
-        if(command.length() != 0) {
-            // todo: throw an exception for too many args
+            addChild(currentNode, commandSplit[i]);
         }
         return currentNode;
+    }
+
+    private void addChild(CommandNode currentNode, String child) {
+        if(isDouble(child)) {
+            currentNode.addChild(myCommandFactory.makeCommand(child));
+        } else {
+            currentNode.addChild(makeNodeTree());
+        }
+        myCurrentCommand = myCurrentCommand.substring(child.length() + 1);
     }
 
     // purpose: check if something successfully can be parsed as a double
