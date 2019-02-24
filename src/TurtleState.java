4e4dc0a9de417173152199b904ca65f499434996
Anna Darwish <ard51@duke.edu>
2019-02-24 14:56:31 -0500

    Added nodes and subscriber basic classes

--- src/TurtleState.java
+++ src/TurtleState.java
@@ -1,2 +1,19 @@
 public class TurtleState {
+    private boolean showTurtle = true;
+    private double xCoor;
+    private double yCoor;
+
+
+    public void updateXCoor(double x){
+        xCoor =x;
+    }
+    public void updateYCoor(double y){
+        yCoor = y;
+    }
+    public void hideTurtle(){
+        showTurtle = false;
+    }
+    public void showTurtle(){
+        showTurtle = true;
+    }
 }
