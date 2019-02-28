b133dbcf47e1c0abb1cccd8f44519c53fc904827
Megan Phibbons <mep76@duke.edu>
2019-02-27 23:14:10 -0500

    finished comment integration

--- back_end/src/parser/Parser.java
+++ back_end/src/parser/Parser.java
@@ -2,6 +2,7 @@ package parser;
 
 import apis.AddVariable;
 import exceptions.InvalidCommandException;
+import exceptions.NothingToRunException;
 import nodes.CommandFactory;
 import nodes.CommandNode;
 
@@ -14,13 +15,17 @@ public class Parser {
     private CommandFactory myCommandFactory;
     private ResourceBundle myParameterProperties;
     private ResourceBundle myCommandProperties;
+    private ResourceBundle mySyntaxProperties;
     private static final String PARAMETER_PROPERTIES_LOCATION = "parser/Parameters";
     private static final String DEFAULT_LANGUAGE = "English";
     private static final String COMMAND_PROPERTIES_LOCATION = "languages/";
+    private static final String SYNTAX_PROPERTIES_LOCATION = "languages/Syntax";
     private String myCurrentCommand;
-    private List<Entry<String, Pattern>> mySymbols;
+    private List<Entry<String, Pattern>> myCommandSyntax;
     private Map<String,Double> myVariables;
-    private List<Entry<String, Pattern>> mySyntax;
+    private List<Entry<String, Pattern>> myNumberOfParameters;
+    private List<Entry<String, Pattern>> myGeneralSyntax;
+    private static final int COMMENT_INDEX = 0;
 
     AddVariable myAddVarFunction = new AddVariable() {
         @Override
@@ -33,14 +38,18 @@ public class Parser {
         myCommandFactory = new CommandFactory();
         myParameterProperties = ResourceBundle.getBundle(PARAMETER_PROPERTIES_LOCATION);
         myCommandProperties = ResourceBundle.getBundle(COMMAND_PROPERTIES_LOCATION + DEFAULT_LANGUAGE);
-        mySymbols = new ArrayList<>();
-        mySyntax = new ArrayList<>();
-        addPatterns(mySymbols, myCommandProperties);
-        addPatterns(mySyntax, myParameterProperties);
+        mySyntaxProperties = ResourceBundle.getBundle(SYNTAX_PROPERTIES_LOCATION);
+        myCommandSyntax = new ArrayList<>();
+        myNumberOfParameters = new ArrayList<>();
+        myGeneralSyntax = new ArrayList<>();
+        addPatterns(myCommandSyntax, myCommandProperties);
+        addPatterns(myNumberOfParameters, myParameterProperties);
+        addPatterns(myGeneralSyntax, mySyntaxProperties);
     }
 
     public List<CommandNode> parse(String input) throws InvalidCommandException { // todo: throw invalidcommandexception and invalidnumberinputs exception
         myCurrentCommand = input;
+        removeComments();
         myVariables = new HashMap<>();
         List<CommandNode> topLevelCommands = new ArrayList<>();
         while(myCurrentCommand.length() > 0) {
@@ -58,7 +67,7 @@ public class Parser {
         String currentValue = commandSplit[0];
         String currentCommandKey = getCommandKey(currentValue);
         int expectedNumberOfParameters = Integer.parseInt(myParameterProperties.getString(currentCommandKey));
-        updateString();
+        updateMyCurrentCommand();
         CommandNode currentNode = myCommandFactory.makeCommand(currentCommandKey);
         for(int i = 1; i <= expectedNumberOfParameters; i++) {
             addChild(currentNode, commandSplit[i]);
@@ -72,7 +81,7 @@ public class Parser {
         } else {
             currentNode.addChild(makeNodeTree());
         }
-        updateString();
+        updateMyCurrentCommand();
     }
 
     // purpose: check if something successfully can be parsed as a double
@@ -86,7 +95,7 @@ public class Parser {
     }
 
     private String getCommandKey(String input) throws InvalidCommandException {
-        for(var symbol : mySymbols) {
+        for(var symbol : myCommandSyntax) {
             if(match(input, symbol.getValue())) {
                 return symbol.getKey();
             }
@@ -101,12 +110,27 @@ public class Parser {
     private void addPatterns(List<Entry<String, Pattern>> patternList, ResourceBundle bundle) {
         patternList.clear();
         for (var key : Collections.list(bundle.getKeys())) {
-            var regex = bundle.getString(key);
+            var regex = bundle.getString(key).trim();
             patternList.add(new SimpleEntry<>(key, Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
         }
     }
 
-    private void updateString() {
+    private boolean isComment(String text) {
+        return match(text, myGeneralSyntax.get(COMMENT_INDEX).getValue());
+    }
+
+    private void removeComments() {
+        String[] lines = myCurrentCommand.split("\\n");
+        for(int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+            if(isComment(lines[i])) {
+                lines[i] = " ";
+            }
+        }
+        myCurrentCommand = String.join(" ", lines).trim();
+    }
+
+    private void updateMyCurrentCommand() {
         String[] split = myCurrentCommand.split(" ");
         myCurrentCommand = "";
         for(int i = 1; i < split.length; i++) {
@@ -117,7 +141,7 @@ public class Parser {
 
     private void updateLanguage(String newLanguage) {
         myCommandProperties = ResourceBundle.getBundle(COMMAND_PROPERTIES_LOCATION + newLanguage);
-        addPatterns(mySymbols, myCommandProperties);
+        addPatterns(myCommandSyntax, myCommandProperties);
     }
 
 }
