4f95d2c18c6e489ead367fa1d7058906e25a7b4c
Anna Darwish <ard51@duke.edu>
2019-03-02 10:54:59 -0500

    Trying to fix set towards

--- back_end/src/nodes/SetTowards.java
+++ back_end/src/nodes/SetTowards.java
@@ -17,15 +17,22 @@ public class SetTowards extends CommandNode {
     public double evaluate(List<ImmutableVisualCommand> myVisCommands, Turtle myTurtle) {
         double newXCoor = super.getChildren().get(0).evaluate(myVisCommands, myTurtle);
         double newYCoor = super.getChildren().get(1).evaluate(myVisCommands, myTurtle);
-        var angle = Math.atan2(newYCoor - myTurtle.getYCoor(), newXCoor - myTurtle.getXCoor() );
-        angle = angle * (180/Math.PI);
-        if(angle < 0)
-        {
-            angle = 360 - (-1.0 * angle);
-        }
-        var newAngle = 90 + angle;
-        myVisCommands.add(new VisualTurtleTurn(newAngle));
-        return Math.abs(myTurtle.getHeading() - newAngle);
+        double angle = Math.atan2(newYCoor - myTurtle.getYCoor(), newXCoor - myTurtle.getXCoor() );
+        System.out.println("RESULT OF ATAN: " + angle * (180.0/Math.PI));
+        System.out.println("CURRENT HEADING: " + myTurtle.getHeading());
+        angle = angle * (180.0/Math.PI);
+        myTurtle.updateHeading(angle);
+//        angle = angle * (180/Math.PI);
+//        if(angle < 0)
+//        {
+//            angle = 360 - (-1.0 * angle);
+//        }
+//        var newAngle = 90 + angle;
+        if (myTurtle.getXCoor() - newXCoor == 0)
+            angle = 180.0;
+        System.out.println(angle);
+        myVisCommands.add(new VisualTurtleTurn(angle));
+        return Math.abs(myTurtle.getHeading() - angle);
     }
     @Override
     public void addChild(CommandNode c){
