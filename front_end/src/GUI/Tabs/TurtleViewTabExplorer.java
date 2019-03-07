e8a409bfc8f7629aa954a835a954243aaf0cf968
Louis Jensen <lbj7@duke.edu>
2019-03-07 16:09:46 -0500

    refactored turtleviewtabexplorer

--- front_end/src/GUI/Tabs/TurtleViewTabExplorer.java
+++ front_end/src/GUI/Tabs/TurtleViewTabExplorer.java
@@ -2,8 +2,6 @@ package GUI.Tabs;
 
 import GUI.Turtle.DisplayView;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-
 import java.util.function.Consumer;
 
 public class TurtleViewTabExplorer extends TabExplorer {
@@ -12,6 +10,7 @@ public class TurtleViewTabExplorer extends TabExplorer {
     public static final String TURTLE_PROPERTIES = "Turtle Properties";
     private SLogoTab myPenProperties;
     private SLogoTab myTurtleProperties;
+    private final double EPSILON = 0.001;
 
     public TurtleViewTabExplorer(){
         myPenProperties = new SLogoTab(PEN_PROPERTIES);
@@ -20,7 +19,7 @@ public class TurtleViewTabExplorer extends TabExplorer {
     }
 
     public Consumer<DisplayView> getTabAccess(){
-        return (x) -> {
+        return x -> {
             updatePenPropertiesTab(x);
             updateTurtleProperties(x);
         };
@@ -29,7 +28,7 @@ public class TurtleViewTabExplorer extends TabExplorer {
     private void updateTurtleProperties(DisplayView x) {
         myTurtleProperties.clearContents();
         myTurtleProperties.addContents(String.format("Heading: %.2f degrees", x.getRotate()));
-        myTurtleProperties.addContents(String.format("Y Coordinate: %.2f", -1 * x.getTranslateY() + 0.001));
+        myTurtleProperties.addContents(String.format("Y Coordinate: %.2f", -1 * x.getTranslateY() + EPSILON));
         myTurtleProperties.addContents(String.format("X Coordinate: %.2f", x.getTranslateX()));
         myTurtleProperties.addContents(String.format("Showing turtle properties for turtle %d", x.getTurtleId()));
     }
