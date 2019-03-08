0e641d076040bb3bd2710e5a5ad9146c910beeeb
Anna Darwish <ard51@duke.edu>
2019-03-07 23:23:39 -0500

    updted user instruction to take in a child node to know name of user instruction

--- back_end/src/nodes/structures/UserInstruction.java
+++ back_end/src/nodes/structures/UserInstruction.java
@@ -11,31 +11,30 @@ import java.util.List;
 
 public class UserInstruction extends CommandNode {
     private UserCreated myUserCreatedItems;
-    private String myUserMethodName;
     public UserInstruction(String n){
         super(n);
-        myUserMethodName = n;
     }
     public UserInstruction(String n, UserCreated user){
         super(n);
         myUserCreatedItems = user;
-        myUserMethodName = n;
     }
     @Override
     public double evaluate(List<ImmutableVisualCommand> myVisCommands, Bale myTurtles) throws InvalidInputException {
-
-        CommandNode listOfVariableValues = super.getChildren().get(0);
+        String myUserMethodName = super.getChildren().get(0).getName();
+        CommandNode listOfVariableValues = super.getChildren().get(1);
         List<Double> myVariableValues = new ArrayList<>();
         for (CommandNode c: listOfVariableValues.getChildren())
             myVariableValues.add(c.evaluate(myVisCommands,myTurtles));
 
         myUserCreatedItems.setUpLocalVariables(myUserMethodName,myVariableValues);
-        CommandNode listOfCommands = super.getChildren().get(1);
+        CommandNode listOfCommands = super.getChildren().get(2);
         double ret = 0.0;
         for (CommandNode c : listOfCommands.getChildren())
             ret = c.evaluate(myVisCommands,myTurtles);
         return ret;
     }
+    @Override
+    public boolean isMethodDeclaration(){return true;}
     @Override
     public boolean needsUserCreated(){
         return true;
