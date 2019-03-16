523a59fffe06202a0d60c31069647d8623755f13
Ryan Culhane <rpc21@duke.edu>
2019-03-16 16:55:58 -0400

    Finished commenting all classes and methods I know

--- front_end/src/GUI/Turtle/DisplayView.java
+++ front_end/src/GUI/Turtle/DisplayView.java
@@ -47,6 +47,10 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         this(new Image(TURTLE_IMAGE));
     }
 
+    /**
+     * DisplayView constructor
+     * @param image image for the DisplayView
+     */
     public DisplayView(Image image){
         super(image);
         setFitHeight(IMAGE_HEIGHT);
@@ -58,33 +62,56 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         setRotate(0);
     }
 
+    /**
+     * DisplayView constructor
+     * @param canvas drawing canvas
+     */
     public DisplayView(Canvas canvas){
         this();
         myCanvas = canvas;
         myContext = myCanvas.getGraphicsContext2D();
     }
 
-    public void passListOfTurtles(List<Integer> listOfTurtles){
-       // myListOfTurtles = listOfTurtles;
-        myListOfActiveTurtles = new TreeSet<Integer>();
-        myListOfActiveTurtles.clear();
-        myListOfActiveTurtles.addAll(listOfTurtles);
-    }
+//    public void passListOfTurtles(List<Integer> listOfTurtles){
+//       // myListOfTurtles = listOfTurtles;
+//        myListOfActiveTurtles = new TreeSet<Integer>();
+//        myListOfActiveTurtles.clear();
+//        myListOfActiveTurtles.addAll(listOfTurtles);
+//    }
 
+    /**
+     * DisplayView constructor
+     * @param displayView displayView to take image from
+     * @param canvas drawing canvas
+     */
     public DisplayView(DisplayView displayView, Canvas canvas){
         this(displayView.getImage(), canvas);
     }
 
+    /**
+     * DisplayView constructor
+     * @param image image to be displayed
+     * @param canvas drawing canvas
+     */
     public DisplayView(Image image, Canvas canvas){
         this(image);
         myCanvas = canvas;
         myContext = myCanvas.getGraphicsContext2D();
     }
 
+    /**
+     * DisplayView constructor
+     * @param displayView DisplayView to be copied
+     */
     public DisplayView(DisplayView displayView){
         this(displayView, displayView.getImage());
     }
 
+    /**
+     * DisplayView constructor
+     * @param displayView DisplayView to be copied
+     * @param image image to be shown for new display view
+     */
     public DisplayView(DisplayView displayView, Image image){
         this(image, displayView.myCanvas);
         copyMoveHistoryAndPen(displayView);
@@ -118,6 +145,10 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         setTranslateY(move.getDisplacement().getY());
     }
 
+    /**
+     * Draw a move on the drawing canvas based on pen properties of the move
+     * @param move move to be drawn
+     */
     public void drawPath(Move move) {
         myContext.setLineWidth(move.getPenWidth());
         myContext.setStroke(move.getPenColor());
@@ -131,16 +162,27 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         myContext.closePath();
     }
 
+    /**
+     * Getter for the pen
+     * @return myPen
+     */
     public Pen getMyPen() {
         return myPen;
     }
 
+    /**
+     * Moves the turtle by x in the x direction and y in the y direction
+     * @param deltaX delta x
+     * @param deltaY delta y
+     */
     public void turtleMove(double deltaX, double deltaY) {
-        System.out.println(getTranslateX() + " " + deltaX + "");
         moveTo(new Point2D(getTranslateX() + deltaX, getTranslateY() + deltaY));
-        System.out.println(getTranslateY());
     }
 
+    /**
+     * Moves the turtle to the new location
+     * @param newLocation next location for the turtle
+     */
     private void moveTo(Point2D newLocation){
         System.out.println("####" +  newLocation);
         Move move = new Move(new Pen(getMyPen()), newLocation);
@@ -149,20 +191,35 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         updateTab();
     }
 
+    /**
+     * Sets rotation of turtle to degrees
+     * @param degrees new rotation of the turtle image
+     */
     public void towards(double degrees) {
         setRotate(degrees);
-        System.out.println("VISUALIZATION :" + getRotate());
     }
 
+    /**
+     * Moves turtle to location x,y
+     * @param x new x position
+     * @param y new y position
+     */
     public void setLocation(double x, double y) {
         System.out.println("**" + x + " " + y);
         moveTo(new Point2D(x, y));
     }
 
+    /**
+     * Moves turtle to 0, 0
+     */
     public void goHome() {
         moveTo(new Point2D(0,0));
     }
 
+    /**
+     * Turns the turtle by degrees number of degrees
+     * @param degrees number of degrees to turn the turtle by
+     */
     public void turn(double degrees) {
         setRotate(getRotate() + degrees);
         updateTab();
@@ -184,6 +241,10 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         setOnContextMenuRequested(e -> myDisplayViewContextMenu.show(this, e.getSceneX(), e.getSceneY()));
     }
 
+    /**
+     * Gives DisplayView ability to update the tab that contains information about the display view
+     * @param tabAccess tab access
+     */
     public void giveTabAccess(Consumer<DisplayView> tabAccess){
         myTabAccess = tabAccess;
         setOnMouseClicked(e -> {
@@ -210,14 +271,23 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         this.setFitHeight(IMAGE_HEIGHT);
     }
 
+    /**
+     * Make turtle active
+     */
     public void setActive(){
         isActive = true;
     }
 
+    /**
+     * Make turtle inactive
+     */
     public void setInActive(){
         isActive = false;
     }
 
+    /**
+     * @return if turtle is active
+     */
     public boolean isItActive(){
         return isActive;
     }
@@ -251,30 +321,53 @@ public abstract class DisplayView extends ImageView implements CommandExecutable
         }
     }
 
+    /**
+     * Sets the turtle's id
+     * @param id new id
+     */
     public void setTurtleId(int id){
         myTurtleId = id;
     }
 
+    /**
+     * Getter for turtle id
+     * @return myTurtleId
+     */
     public int getTurtleId(){
         return myTurtleId;
     }
 
+    /**
+     * Updates the tab of DisplayView information
+     */
     private void updateTab(){
         myTabAccess.accept(this);
     }
 
+    /**
+     * Change pen color for turtle
+     * @param color new color
+     */
     public void setPenColor(Paint color){
         myPen.setMyColor(color);
         updateTab();
-    };
+    }
 
+    /**
+     * Change pen width for this turtle's pen
+     * @param pixels new pen width
+     */
     public void setPenWidth(double pixels){
         myPen.setMyWidth(pixels);
         updateTab();
-    };
+    }
 
+    /**
+     * Change whether pen is up or down
+     * @param isDown whether pen should be down
+     */
     public void setPenDown(boolean isDown){
         myPen.setDown(isDown);
         updateTab();
-    };
+    }
 }
